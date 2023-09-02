80eff580b7ff7159db6809f313afbf578bad98d5
Viktor Szépe <viktor@szepe.net>
2023-09-02 10:50:32 +0000

    Add script meta header

--- snapshot.sh
+++ snapshot.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# Make a snapshot of listed pages in a normalized form
+#
+# DEPENDS       :apt-get install wget dos2unix xmlstarlet libxml2-utils openjdk-17-jre-headless
+# DEPENDS       :https://github.com/validator/validator
 
 set -e
 
@@ -14,12 +19,13 @@ while read -r URL; do
     grep -q -F -i '</html>' "${SLUG}.html"
     # Fixes
     sed -i -e 's#pswp__preloader--active#pswp__preloader- -active#' "${SLUG}.html"
-    #java -jar vnu.jar --Werror "${SLUG}.html"
+    java -jar vnu.jar --Werror "${SLUG}.html"
     dos2unix -k -n "${SLUG}.html" "${SLUG}.html.lf"
     expand --tabs=4 "${SLUG}.html.lf" >"${SLUG}.html.lf.space"
     xmlstarlet fo --html --recover "${SLUG}.html.lf.space" >"${SLUG}.xml.original" 2>/dev/null
+    # Fixes
     #xmlstarlet ed --delete '//path'
     xmllint --format --output "${SLUG}.xml" "${SLUG}.xml.original"
-done < urls
+done <urls
 
 ls -1 -t -r ./*.xml
