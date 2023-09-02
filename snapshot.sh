615323838de24ec3a125d7d075d5ea423699948a
Viktor Szépe <viktor@szepe.net>
2023-09-02 10:40:45 +0000

    Add sample script

---
+++ snapshot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+DIRECTORY="$(date "+%s")"
+mkdir "${DIRECTORY}"
+cd "${DIRECTORY}"
+
+wget "https://EXAMPLE.COM/.well-known/safe-upgrade/urls"
+
+while read -r URL; do
+    SLUG="$(echo "${URL}" | iconv -t "ascii//TRANSLIT" | sed -e 's#[^0-9A-Za-z-]#_#g')"
+    wget -qSO- "${URL}" 1>"${SLUG}.html" 2>"${SLUG}.headers"
+    grep -q -F -i '</html>' "${SLUG}.html"
+    # Fixes
+    sed -i -e 's#pswp__preloader--active#pswp__preloader- -active#' "${SLUG}.html"
+    #java -jar vnu.jar --Werror "${SLUG}.html"
+    dos2unix -k -n "${SLUG}.html" "${SLUG}.html.lf"
+    expand --tabs=4 "${SLUG}.html.lf" >"${SLUG}.html.lf.space"
+    xmlstarlet fo --html --recover "${SLUG}.html.lf.space" >"${SLUG}.xml.original" 2>/dev/null
+    #xmlstarlet ed --delete '//path'
+    xmllint --format --output "${SLUG}.xml" "${SLUG}.xml.original"
+done < urls
+
+ls -1 -t -r ./*.xml
