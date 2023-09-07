c2e1f9090051c060dd929c13a7736dbc04fb1185
Viktor Szépe <viktor@szepe.net>
2023-09-07 17:58:37 +0000

    Improve script

--- snapshot.sh
+++ snapshot.sh
@@ -16,18 +16,18 @@ wget "https://EXAMPLE.COM/.well-known/safe-upgrade/urls"
 
 while read -r URL; do
     SLUG="$(echo "${URL}" | iconv -t "ascii//TRANSLIT" | sed -e 's#[^0-9A-Za-z-]#_#g')"
-    # Add --post-data= to make a POST request
+    # Add --post-data= to make POST requests to bypass caches
     wget --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0" \
-        -q -S -O- "${URL}" 1>"${SLUG}.html" 2>"${SLUG}.headers"
+        -q -S -O - "${URL}" 1>"${SLUG}.html" 2>"${SLUG}.headers"
     grep -q -F -i '</html>' "${SLUG}.html"
     # Fixes
-    sed -i -e 's#pswp__preloader--active#pswp__preloader- -active#' "${SLUG}.html"
+    sed -i -e 's#type="[0-9a-f]\{24\}-text/javascript"#type="ROCKET_LOADER_ID-text/javascript"#' "${SLUG}.html"
     java -jar vnu.jar --errors-only --filterpattern 'Element “div” not allowed.*' "${SLUG}.html"
     dos2unix -q -k -n "${SLUG}.html" "${SLUG}.html.lf"
     expand --tabs=4 "${SLUG}.html.lf" >"${SLUG}.html.lf.space"
     xmlstarlet fo --html --recover "${SLUG}.html.lf.space" >"${SLUG}.xml.original" 2>/dev/null
     # Remove always changing elements
-    #xmlstarlet ed --delete '//path'
+    #xmlstarlet ed --update '//script[contains(@type,"-text/javascript")]/@type' -v 'ROCKET_LOADER_ID-text/javascript'
     xmllint --format --output "${SLUG}.xml" "${SLUG}.xml.original"
     sed -e "0,/<head>/{s#<head>#&<base href='${URL}'>#}" "${SLUG}.html" >"${SLUG}.base.html"
     chromium --disable-crash-reporter --disable-audio-output --disable-gpu --single-process \
